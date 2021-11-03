f109a14e9d668cf3aed402ffaf181611173671e3
Arseniy Shevchenko <shevchenkoav2002@gmail.com>
2021-11-03 13:29:16 +0300

    fixed

--- Lab2/Lab2.sql
+++ Lab2/Lab2.sql
@@ -1,72 +1,91 @@
-/*1.*/	
-SELECT Color, Count(*) FROM Production.Product
-WHERE ListPrice>=30
+/*1.*/
+SELECT Color, Count(*)
+FROM Production.Product
+WHERE ListPrice >= 30
 GROUP BY COLOR
 
 /*2.*/
-SELECT Color FROM Production.Product
+SELECT Color
+FROM Production.Product
 GROUP BY (Color)
-HAVING MIN(ListPrice)>100
-/*3.*/	
-SELECT ProductSubcategoryID, COUNT(*) FROM Production.Product
+HAVING MIN(ListPrice) > 100
+/*3.*/
+SELECT ProductSubcategoryID, COUNT(*)
+FROM Production.Product
 GROUP BY ProductSubcategoryID
-/*4.*/	
-SELECT ProductID, COUNT(*) FROM Sales.SalesOrderDetail
+/*4.*/
+SELECT ProductID, COUNT(*)
+FROM Sales.SalesOrderDetail
 GROUP BY ProductID
 /*5.*/
-SELECT ProductID, COUNT(*) FROM Sales.SalesOrderDetail
+SELECT ProductID, COUNT(*)
+FROM Sales.SalesOrderDetail
 GROUP BY ProductID
-HAVING COUNT(*)>5
+HAVING COUNT(*) > 5
 
 /*6.*/
-SELECT CustomerID FROM Sales.SalesOrderHeader
-GROUP BY CustomerID, DATEPART(DAYOFYEAR,OrderDate)
-HAVING COUNT(*)>1
+SELECT CustomerID
+FROM Sales.SalesOrderHeader
+GROUP BY CustomerID, DATEPART(DAYOFYEAR, OrderDate)
+HAVING COUNT(*) > 1
 
 /*7.*/
-SELECT SalesOrderID FROM Sales.SalesOrderDetail
+SELECT SalesOrderID
+FROM Sales.SalesOrderDetail
 GROUP BY SalesOrderID
-HAVING COUNT(*)>3
+HAVING COUNT(*) > 3
 /*8.*/
-SELECT ProductID FROM Sales.SalesOrderDetail
+SELECT ProductID
+FROM Sales.SalesOrderDetail
 GROUP BY ProductID
-HAVING COUNT(*)>3
+HAVING COUNT(*) > 3
 /*9.*/
-SELECT ProductID FROM Sales.SalesOrderDetail
+SELECT ProductID
+FROM Sales.SalesOrderDetail
 GROUP BY ProductID
-HAVING COUNT(*)=3 OR COUNT(*)=5
-/*10.*/	
-SELECT ProductSubcategoryID, COUNT(*) FROM Production.Product
+HAVING COUNT(*) = 3
+    OR COUNT(*) = 5
+/*10.*/
+SELECT ProductSubcategoryID, COUNT(*)
+FROM Production.Product
 GROUP BY ProductSubcategoryID
-HAVING COUNT(*)>10
+HAVING COUNT(*) > 10
 
 /*11.*/
-SELECT ProductID FROM Sales.SalesOrderDetail
+SELECT ProductID
+FROM Sales.SalesOrderDetail
 GROUP BY ProductID
-HAVING MAX(OrderQty)=1
+HAVING MAX(OrderQty) = 1
 ORDER BY ProductID ASC
 /*12.*/
-SELECT TOP 1 SalesOrderID FROM Sales.SalesOrderDetail
+SELECT TOP 1 SalesOrderID
+FROM Sales.SalesOrderDetail
 GROUP BY SalesOrderID
 ORDER BY COUNT(ProductID) DESC
 
 /*13.*/
-SELECT SalesOrderID FROM Sales.SalesOrderDetail
+SELECT SalesOrderID
+FROM Sales.SalesOrderDetail
 GROUP BY SalesOrderID
-ORDER BY SUM(OrderQty*UnitPrice) DESC
+ORDER BY SUM(OrderQty * UnitPrice) DESC
 
 /*14*/
-SELECT ProductSubcategoryID FROM Production.Product
-Where ProductSubcategoryID IS NOT NULL AND Color IS NOT NULL
+SELECT ProductSubcategoryID
+FROM Production.Product
+Where ProductSubcategoryID IS NOT NULL
+  AND Color IS NOT NULL
 GROUP BY ProductSubcategoryID
 /*15.*/
-SELECT Color, COUNT(*) FROM Production.Product
+SELECT Color, COUNT(*)
+FROM Production.Product
 WHERE COLOR IS NOT NULL
 GROUP BY Color
 ORDER BY COUNT(*) DESC
 
 /*16.*/
-SELECT ProductID FROM Sales.SalesOrderDetail
+SELECT ProductID
+FROM Sales.SalesOrderDetail
 GROUP BY ProductID
-HAVING MAX(OrderQty)>1 AND COUNT(SalesOrderID)>2
+HAVING MAX(OrderQty) > 1
+   AND COUNT(SalesOrderID) > 2
 ORDER BY ProductID ASC
--- Lab3/Lab3.sql
+++ Lab3/Lab3.sql
@@ -1,76 +1,89 @@
 /*1.*/
-SELECT P.NAME, PC.Name FROM Production.Product AS P
-JOIN Production.ProductSubcategory as PSC
-ON PSC.ProductCategoryID=P.ProductSubcategoryID
-JOIN Production.ProductCategory AS PC
-ON PC.ProductCategoryID=PSC.ProductCategoryID
-WHERE P.Color='Red' AND P.ListPrice>=100
+SELECT P.NAME, PC.Name
+FROM Production.Product AS P
+         JOIN Production.ProductSubcategory as PSC
+              ON PSC.ProductCategoryID = P.ProductSubcategoryID
+         JOIN Production.ProductCategory AS PC
+              ON PC.ProductCategoryID = PSC.ProductCategoryID
+WHERE P.Color = 'Red'
+  AND P.ListPrice >= 100
 /*2.*/
-SELECT P.NAME, P1.Name FROM Production.ProductSubcategory AS P
-JOIN Production.ProductSubcategory as P1
-ON P1.Name=P.Name AND P1.ProductSubcategoryID!=P.ProductSubcategoryID
+SELECT P.NAME, P1.Name
+FROM Production.ProductSubcategory AS P
+         JOIN Production.ProductSubcategory as P1
+              ON P1.Name = P.Name AND P1.ProductSubcategoryID!=P.ProductSubcategoryID
 /*3.*/
-SELECT PC.NAME, COUNT(*) FROM Production.Product AS P
-JOIN Production.ProductSubcategory as PSC
-ON PSC.ProductSubcategoryID=P.ProductSubcategoryID 
-JOIN Production.ProductCategory AS PC
-ON PSC.ProductCategoryID=PC.ProductCategoryID
+SELECT PC.NAME, COUNT(*)
+FROM Production.Product AS P
+         JOIN Production.ProductSubcategory as PSC
+              ON PSC.ProductSubcategoryID = P.ProductSubcategoryID
+         JOIN Production.ProductCategory AS PC
+              ON PSC.ProductCategoryID = PC.ProductCategoryID
 GROUP BY PC.Name
 /*4.*/
-SELECT PSC.ProductSubcategoryID, PSC.NAME, COUNT(*) FROM Production.Product AS P
-JOIN Production.ProductSubcategory as PSC
-ON PSC.ProductSubcategoryID=P.ProductSubcategoryID 
+SELECT PSC.ProductSubcategoryID, PSC.NAME, COUNT(*)
+FROM Production.Product AS P
+         JOIN Production.ProductSubcategory as PSC
+              ON PSC.ProductSubcategoryID = P.ProductSubcategoryID
 GROUP BY PSC.ProductSubcategoryID, PSC.Name
 /*5.*/
-SELECT TOP 3 PSC.NAME, COUNT(*) AS AMOUNT FROM Production.Product AS P
-JOIN Production.ProductSubcategory as PSC
-ON PSC.ProductSubcategoryID=P.ProductSubcategoryID 
+SELECT TOP 3 PSC.NAME, COUNT(*) AS AMOUNT
+FROM Production.Product AS P
+         JOIN Production.ProductSubcategory as PSC
+              ON PSC.ProductSubcategoryID = P.ProductSubcategoryID
 GROUP BY PSC.Name
 ORDER BY AMOUNT DESC
 /*6.*/
-SELECT PSC.NAME, MAX(ListPrice) AS AMOUNT FROM Production.Product AS P
-JOIN Production.ProductSubcategory as PSC
-ON PSC.ProductSubcategoryID=P.ProductSubcategoryID 
-WHERE P.Color='Red'
+SELECT PSC.NAME, MAX(ListPrice) AS AMOUNT
+FROM Production.Product AS P
+         JOIN Production.ProductSubcategory as PSC
+              ON PSC.ProductSubcategoryID = P.ProductSubcategoryID
+WHERE P.Color = 'Red'
 GROUP BY PSC.Name
 /*7.*/
-SELECT V.Name, COUNT(ProductID) FROM Purchasing.ProductVendor as PV 
-JOIN Purchasing.Vendor as V
-ON PV.BusinessEntityID=V.BusinessEntityID
+SELECT V.Name, COUNT(ProductID)
+FROM Purchasing.ProductVendor as PV
+         JOIN Purchasing.Vendor as V
+              ON PV.BusinessEntityID = V.BusinessEntityID
 GROUP BY V.Name
 /*8.*/
-SELECT P.Name FROM Production.Product as P 
-JOIN Purchasing.ProductVendor as PV
-ON PV.ProductID=P.ProductID
+SELECT P.Name
+FROM Production.Product as P
+         JOIN Purchasing.ProductVendor as PV
+              ON PV.ProductID = P.ProductID
 GROUP BY P.Name
-HAVING COUNT(PV.BusinessEntityID)>1
-/*9.*/	
-SELECT TOP 1 P.Name, COUNT(*) FROM Purchasing.PurchaseOrderDetail as POD 
-JOIN Production.Product as P
-ON P.ProductID=POD.ProductID
+HAVING COUNT(PV.BusinessEntityID) > 1
+/*9.*/
+SELECT TOP 1 P.Name, COUNT(*)
+FROM Purchasing.PurchaseOrderDetail as POD
+         JOIN Production.Product as P
+              ON P.ProductID = POD.ProductID
 GROUP BY P.Name
 ORDER BY COUNT(*) DESC
-/*10.*/	
-SELECT TOP 1 PC.Name, COUNT(*) FROM Purchasing.PurchaseOrderDetail as POD 
-JOIN Production.Product as P
-ON P.ProductID=POD.ProductID
-JOIN Production.ProductSubcategory as PSC
-ON P.ProductSubcategoryID=PSC.ProductSubcategoryID
-JOIN Production.ProductCategory as PC
-ON PC.ProductCategoryID=PSC.ProductCategoryID
+/*10.*/
+SELECT TOP 1 PC.Name, COUNT(*)
+FROM Purchasing.PurchaseOrderDetail as POD
+         JOIN Production.Product as P
+              ON P.ProductID = POD.ProductID
+         JOIN Production.ProductSubcategory as PSC
+              ON P.ProductSubcategoryID = PSC.ProductSubcategoryID
+         JOIN Production.ProductCategory as PC
+              ON PC.ProductCategoryID = PSC.ProductCategoryID
 GROUP BY PC.Name
 ORDER BY COUNT(*) DESC
 /*11.*/
-SELECT PC.Name, COUNT(DISTINCT PSC.ProductSubcategoryID), COUNT(P.ProductID) FROM Production.Product as P 
-JOIN Production.ProductSubcategory as PSC
-ON P.ProductSubcategoryID=PSC.ProductSubcategoryID
-JOIN Production.ProductCategory as PC
-ON PC.ProductCategoryID=PSC.ProductCategoryID
+SELECT PC.Name, COUNT(DISTINCT PSC.ProductSubcategoryID), COUNT(P.ProductID)
+FROM Production.Product as P
+         JOIN Production.ProductSubcategory as PSC
+              ON P.ProductSubcategoryID = PSC.ProductSubcategoryID
+         JOIN Production.ProductCategory as PC
+              ON PC.ProductCategoryID = PSC.ProductCategoryID
 GROUP BY PC.Name
 
 /*12.*/
-SELECT V.CreditRating, COUNT(DISTINCT PV.ProductID) FROM Purchasing.Vendor as V 
-JOIN Purchasing.ProductVendor as PV
-ON V.BusinessEntityID=PV.BusinessEntityID
+SELECT V.CreditRating, COUNT(DISTINCT PV.ProductID)
+FROM Purchasing.Vendor as V
+         JOIN Purchasing.ProductVendor as PV
+              ON V.BusinessEntityID = PV.BusinessEntityID
 GROUP BY V.CreditRating
 
